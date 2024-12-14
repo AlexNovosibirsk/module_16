be640a3b4d1b519bad11e4230302c18e7a8abc11
AlexGerasimov <43001679+AlexNovosibirsk@users.noreply.github.com>
2024-12-14 11:14:53 +0700

    "Модели данных Pydantic"

--- module_16_4.py
+++ module_16_4.py
@@ -1,6 +1,5 @@
 # Задача "Модель пользователя":
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi import HTTPException
 from pydantic import BaseModel, Field
 from typing import List
@@ -14,7 +13,12 @@ class User(BaseModel):
     age: int
 
 
-users: List = [User(id=1, username="ASD", age=22)]
+class UserCreate(BaseModel):
+    username: str = Field(..., min_length=5, max_length=25, description="Имя пользователя")
+    age: int = Field(ge=18, le=100, description="Возраст пользователя")
+
+
+users: List = [User(id=1, username="Ivan Co", age=22)]
 
 
 @app.get("/users", response_model=List[User])
@@ -22,30 +26,33 @@ async def get_users():
     return users
 
 
-@app.post("/user/{username}/{age}", response_model=User)
-async def create_user(username, age):
+@app.post("/user", response_model=User)
+async def create_user(user: UserCreate):
     new_id = max((user.id for user in users), default=0) + 1
-    new_user = User(id=new_id, username=username, age=age)
+    new_user = UserCreate(id=new_id, username=user.username, age=user.age)
     users.append(new_user)
     return new_user
 
 
-@app.put("/user/{user_id}/{username}/{age}", response_model=User)
-async def update_task(user_id: int, username: str, age: int):
-    for user in users:
-        if user.id == user_id:
-            user.username = username
-            user.age = age
-            return user
+@app.put("/user/{user_id}", response_model=User)
+async def update_user(user_id: int, username: str = Body(), age: int = Body()):
+    for user_obj in users:
+        if user_obj.id == user_id:
+            user_obj.username = username
+            user_obj.age = age
+            return user_obj
     raise HTTPException(status_code=404, detail="User was not found")
 
 
 @app.delete("/user/{user_id}", response_model=User)
-async def delete_task(user_id: int):
-    for i, user in enumerate(users):
-        if user.id == user_id:
+async def delete_user(user_id: int):
+    for i, user_obj in enumerate(users):
+        if user_obj.id == user_id:
             del users[i]
-            return user
+            return user_obj
     raise HTTPException(status_code=404, detail="User was not found")
 
+
 # uvicorn module_16_4:app --reload
+
+
