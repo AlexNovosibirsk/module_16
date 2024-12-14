d2cf473fcac9b54dcb9dcdd821e8ce735a5ace86
AlexGerasimov <43001679+AlexNovosibirsk@users.noreply.github.com>
2024-12-14 14:01:05 +0700

    "Модели данных Pydantic"

--- module_16_4.py
+++ module_16_4.py
@@ -1,24 +1,22 @@
 # Задача "Модель пользователя":
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from fastapi import HTTPException
 from pydantic import BaseModel, Field
 from typing import List
 
 app = FastAPI()
 
-
 class User(BaseModel):
     id: int
-    username: str
-    age: int
-
+    username: str = Field(..., min_length=5, max_length=25, description="Имя пользователя")
+    age: int = Field(ge=18, le=100, description="Возраст пользователя")
 
 class UserCreate(BaseModel):
     username: str = Field(..., min_length=5, max_length=25, description="Имя пользователя")
     age: int = Field(ge=18, le=100, description="Возраст пользователя")
 
-
 users: List = [User(id=1, username="Ivan Co", age=22)]
+# users: List = []
 
 
 @app.get("/users", response_model=List[User])
@@ -26,20 +24,20 @@ async def get_users():
     return users
 
 
-@app.post("/user", response_model=User)
+@app.post("/user")
 async def create_user(user: UserCreate):
     new_id = max((user.id for user in users), default=0) + 1
-    new_user = UserCreate(id=new_id, username=user.username, age=user.age)
+    new_user = User(id=new_id, username=user.username, age=user.age)
     users.append(new_user)
     return new_user
 
 
 @app.put("/user/{user_id}", response_model=User)
-async def update_user(user_id: int, username: str = Body(), age: int = Body()):
+async def update_user(user_id: int, user: User):
     for user_obj in users:
         if user_obj.id == user_id:
-            user_obj.username = username
-            user_obj.age = age
+            user_obj.username = user.username
+            user_obj.age = user.age
             return user_obj
     raise HTTPException(status_code=404, detail="User was not found")
 
@@ -54,5 +52,4 @@ async def delete_user(user_id: int):
 
 
 # uvicorn module_16_4:app --reload
-
 
